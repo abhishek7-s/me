ee211d024372c45d41ebcd79c5dc8af49151d1aa
Abhishek <abhisheksharma7124@gmail.com>
2023-11-21 20:09:12 +0530

    Added some animation

--- JS/script.js
+++ JS/script.js
@@ -15,3 +15,31 @@ darkbtn.addEventListener('click', () => {
     main.classList.toggle('dark');
     navList.classList.toggle('darknav');
 })
+
+
+
+
+
+
+function reveal(className) {
+  
+  var reveals = document.querySelectorAll("."+className);
+    for (var i = 0; i < reveals.length; i++) {
+      var windowHeight = window.innerHeight;
+      var elementTop = reveals[i].getBoundingClientRect().top;
+      var elementVisible = 150;
+  
+      if (elementTop < windowHeight - elementVisible) {
+        reveals[i].classList.add("active");
+      } else {
+        reveals[i].classList.remove("active");
+      }
+    }
+  }
+  
+  window.addEventListener("scroll", function(){ reveal("abtme") });
+  window.addEventListener("scroll", function(){ reveal("pcpic") });
+  window.addEventListener("scroll", function(){ reveal("skillH1") });
+  window.addEventListener("scroll", function(){ reveal("ls") });
+  window.addEventListener("scroll", function(){ reveal("projectH1") });
+  window.addEventListener("scroll", function(){ reveal("project") });
